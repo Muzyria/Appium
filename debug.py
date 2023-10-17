0796abebbfd43b62c536968981194a06269369b8
“Muzyria” <fila090580@gmail.com>
2023-10-17 14:35:05 +0300

    new

---
+++ debug.py
@@ -0,0 +1,17 @@
+import os
+import time
+import unittest
+from appium import webdriver
+from appium.webdriver.common.appiumby import By
+
+
+os.system(f'adb connect 192.168.3.221')
+
+capabilities = dict(
+    platformName='android',
+    automationName='uiautomator2',
+    deviceName='192.168.3.221'
+)
+
+driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', capabilities)
+driver.implicitly_wait(10)
