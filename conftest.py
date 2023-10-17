5bad16f19838dcc1e0943f822b53c845e02cd34b
“Muzyria” <fila090580@gmail.com>
2023-10-17 16:56:00 +0300

    new

--- conftest.py
+++ conftest.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 from selenium import webdriver as selenium_webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -13,19 +15,40 @@ def selenium_driver():
     yield sel_driver
     sel_driver.quit()
 
+# from selenium.webdriver import ActionChains
+# from selenium.webdriver.support.ui import WebDriverWait as wait
+# from selenium.webdriver.support import expected_conditions as EC
+#
+#
+# class BasePage:
+#     def __init__(self, driver, url):
+#         self.driver = driver
+#         self.url = url
+#
+#     def open(self):
+#         self.driver.get(self.url)
+#
+#
+# def test(selenium_driver):
+#     page = BasePage(selenium_driver, 'https://google.com')
+#     page.open()
+#     time.sleep(3)
 
-# Фикстура для Appium
-@pytest.fixture(scope="function")
-def appium_driver():
-    capabilities = dict(
-        platformName='android',
-        automationName='uiautomator2',
-        deviceName=''
-    )
 
-    app_driver = appium_webdriver.Remote('http://127.0.0.1:4723/wd/hub', capabilities)
-    yield app_driver
-    app_driver.quit()
+
+
+# # Фикстура для Appium
+# @pytest.fixture(scope="function")
+# def appium_driver():
+#     capabilities = dict(
+#         platformName='android',
+#         automationName='uiautomator2',
+#         deviceName=''
+#     )
+#
+#     app_driver = appium_webdriver.Remote('http://127.0.0.1:4723/wd/hub', capabilities)
+#     yield app_driver
+#     app_driver.quit()
 
 
 # def test_appium_example(appium_driver):
--- pages/app_base_page.py
+++ pages/app_base_page.py
@@ -3,10 +3,10 @@ from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-# class APPBasePage:
-#     def __init__(self, driver, url):
-#         self.driver = driver
-#         self.url = url
+class APPBasePage:
+    def __init__(self, driver):
+        self.driver = driver
+
 #
 #     def open(self):
 #         self.driver.get(self.url)
