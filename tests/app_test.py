c8037edcdfda6558894fcb390f14a6ec5a82a71b
“Muzyria” <fila090580@gmail.com>
2023-11-14 18:04:34 +0200

    new

--- tests/app_test.py
+++ tests/app_test.py
@@ -16,47 +16,6 @@ class TestFirst:
             el = Page(appium_driver)
             el.first()
 
-        # def test_click_flag(self, appium_driver):
-        #     el = Page(appium_driver)
-        #     el.click_flag()
-        #
-        # def test_swipe(self, appium_driver):
-        #     el = Page(appium_driver)
-        #     el.swipe_my_down()
-        #
-        # def test_swipe_up(self, appium_driver):
-        #     el = Page(appium_driver)
-        #     el.swipe_my_up()
-        #
-        # def test_press_key(self, appium_driver):
-        #     el = Page(appium_driver)
-        #
-        #     # el.press_key(3)
-        #     el.long_press_key(26)
-        #     time.sleep(5)
-        #
-        # def test_inter_android_menu(self, appium_driver):
-        #     el = Page(appium_driver)
-        #     el.android_menu()
-        #
-        #     el.go_to_restart()
-        #
-        # def test_try_make_screenshot(self, appium_driver):
-        #     el = Page(appium_driver)
-        #     el.try_make_screenshot()
-        #
-        # def test_try_make_element_screenshot(self, appium_driver):
-        #     el = Page(appium_driver)
-        #     el.try_make_element_screenshot()
-        #
-        # def test_try_check_element_visible(self, appium_driver):
-        #     el = Page(appium_driver)
-        #     el.try_check_visible_element()
-        #
-        # def test_enter_settings_yamatrak(self, appium_driver):
-        #     el = Page(appium_driver)
-        #     el.from_main_page_to_settings()
-
         def test_a(self, appium_driver, selenium_driver):
             device = Page(appium_driver)
 
