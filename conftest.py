971bac42808ca6e7dea1d6ca6f62cbcdced3a007
“Muzyria” <fila090580@gmail.com>
2024-03-25 16:14:15 +0200

    refactor new tests

--- conftest.py
+++ conftest.py
@@ -1,24 +1,41 @@
 
 import pytest
-from selenium import webdriver as selenium_webdriver
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
 from appium import webdriver as appium_webdriver
 
 
-# Фикстура для Selenium
+def pytest_addoption(parser):
+    """Опции командной строки.
+    В командную строку передается параметр вида '--language="es"'
+    По умолчанию передается параметр, включающий английский интерфейс в браузере
+    """
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language: ---")
+
+
 @pytest.fixture(scope="function")
-def selenium_driver():
-    print('Start Selenium Fixture')
-    sel_driver = selenium_webdriver.Chrome(ChromeDriverManager().install())
-    sel_driver.maximize_window()
-    yield sel_driver
-    sel_driver.quit()
+def browser(request):
+    """selenium fixture"""
+    print("__USE_SELENIUM_FIXTURE__")
+    print("\nstart browser for test..")
+    options = Options()
+    user_language = request.config.getoption("language")
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
 
 
 # Фикстура для Appium
 @pytest.fixture(scope="function")
 def appium_driver():
-    print('Start Appium Fixture')
+    """appium fixture"""
+    print('__USE_APPIUM_FIXTURE__')
     capabilities = dict(
         platformName='android',
         automationName='uiautomator2',
@@ -28,35 +45,3 @@ def appium_driver():
     app_driver = appium_webdriver.Remote('http://127.0.0.1:4723/wd/hub', capabilities)
     yield app_driver
     app_driver.quit()
-
-
-
-"""
-import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
-
-
-def pytest_addoption(parser):
-    '''Опции командной строки.
-    В командную строку передается параметр вида '--language="es"'
-    По умолчанию передается параметр, включающий английский интерфейс в браузере
-    '''
-    parser.addoption('--language', action='store', default='en',
-                     help="Choose language: ---")
-
-
-@pytest.fixture(scope="function")
-def browser(request):
-    print("___USE MAIN FIXTURE__")
-    print("\nstart browser for test..")
-    options = Options()
-    user_language = request.config.getoption("language")
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
-"""
--- tests/test_device_send_message.py
+++ tests/test_device_send_message.py
@@ -1,6 +1,5 @@
-import time
 
-from pages.app_steps_page import AutomaticAPKOSUpdate
+
 
 from pages.adb_commands import BaseAdbCommands
 
@@ -8,26 +7,5 @@ from pages.adb_commands import BaseAdbCommands
 
 
 class TestFirst:
-    class TestAppFirst:
-        adb_device = BaseAdbCommands('192.168.0.105')
-        adb_device.check_devices_active()
+    pass
 
-        def test_case_one(self,  selenium_driver, appium_driver):
-            device = AutomaticAPKOSUpdate(appium_driver)
-
-            # web_syncwise = TestControlSyncWise.TestSelSecond(**payloads_syncwise_live['superior'])
-            # web_syncwise.test_check_assets_details(selenium_driver)
-
-            # web_control = TestControlSyncWise.TestSelFirst(**payloads_control_dev['superior'])
-            # web_control.test_choose_app(selenium_driver)
-
-            # web_control.test_remove_app(selenium_driver)
-            # web_control.test_choose_app(selenium_driver)
-
-            apk_version = device.case_one_steps()
-            print(f'APK VERSION {apk_version}')
-            assert apk_version == "2.1.77"
-
-            # web_control.test_remove_app(selenium_driver)
-
-            # self.adb_device.device_close_yamatack()
