caa96876d666f82a118cfe18f5b19e3c08cdd1d1
FILA <fila090580@gmail.com>
2024-03-26 17:24:51 +0200

    new

--- pages/web_pages/course_map_page.py
+++ pages/web_pages/course_map_page.py
@@ -1,3 +1,4 @@
+import time
 
 from pages.base_page import BasePage
 from locators.web_locators import CourseMapPageLocators
@@ -7,4 +8,5 @@ class CourseMapPage(BasePage):
     def check_incoming_message(self):
         text_message = self.element_is_visible(CourseMapPageLocators.TEXT_MESSAGE).text
         print(f"-------{text_message}--------")
+        time.sleep(10)
         self.element_is_visible(CourseMapPageLocators.BUTTON_CONFIRMED_MESSAGE).click()
