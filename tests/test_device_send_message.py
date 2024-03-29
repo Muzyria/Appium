7ec4bf24a6913fd38ee388c588219cd3021f5ea4
FILA <fila090580@gmail.com>
2024-03-30 01:40:39 +0200

    new ++

--- tests/test_device_send_message.py
+++ tests/test_device_send_message.py
@@ -11,7 +11,6 @@ from pages.device_pages.select_language_page import SelectLanguageScreen
 
 
 class TestSendMessages:
-
     @pytest.fixture(scope="session", autouse=True)
     def login_to_site(self, driver):
         link = "https://beta.syncwise360.com/login"
@@ -20,20 +19,23 @@ class TestSendMessages:
         self.login_page.enter_user_to_site()  # входим на сайт
         time.sleep(10)
 
-    @pytest.mark.skip
-    @pytest.mark.parametrize('number_of_language', list(range(1, 9)))
-    def test_device_select_language(self, appium_driver, number_of_language):
+    @pytest.fixture(scope="function", params=list(range(1, 9)))
+    def setup_language(self, appium_driver, request):
+        number_of_language = request.param
+        self.no_test_device_select_language(appium_driver, number_of_language)
+        yield
+
+    def no_test_device_select_language(self, appium_driver, number_of_language):
         self.screen = MainScreen(appium_driver)                         # инициализируем Main screen
         self.screen.press_button_select_language()                      # press button select language
 
         self.language_screen = SelectLanguageScreen(appium_driver)      # инициализируем Select Language screen
         self.language_screen.select_language(number_of_language)        # press button language
         time.sleep(1)
-        yield self
 
     @pytest.mark.device
-    @pytest.mark.parametrize('number_of_message', list(range(1, 3)))
-    def test_send_device_message(self, appium_driver, driver, number_of_message):
+    @pytest.mark.parametrize('number_of_message', list(range(1, 9)))
+    def test_send_device_message(self, appium_driver, driver, number_of_message, setup_language):
         self.screen = MainScreen(appium_driver)                         # инициализируем Main screen
         self.screen.press_button_menu()                                 # Press button menu
 
